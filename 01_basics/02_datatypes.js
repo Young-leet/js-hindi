8340b93d47c719768ad11f0c09d1356e443016e6
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-22 08:09:28 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//alert('hello') we are using node js not browser
+
+console.log('readibility')
+
+console.log(typeof null) // null is an object datatype
+console.log(typeof undefined) // undefined is of undefiend type
