aed812db7a62c0a9ab427d436b037ef94db46699
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-22 08:29:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,34 @@
+let score = true
+
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number (score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// '33' = 33
+// '33abc' = NaN
+// true => 1 , false = 0
+// 
+
+let isLoggedin = 0
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(booleanIsLoggedIn)
+
+/* 
+1 => true
+0 => false
+'' => false
+'abc' => true
+*/
+
+
+let someNum = 25
+let stringNum = String(someNum)
+console.log(typeof stringNum)
+
+
