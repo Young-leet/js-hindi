ff2b9e7cdfccf28a440220283a99a215446f8d8b
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-22 07:51:16 +0000

    js variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId  = 144553
+let accountEmail = 'gitesh@gmail.com'
+var accountPassword = "1235"
+accountCity = "jaipur"
+let accountState 
+
+
+
+
+//accountId = 2
+console.log(accountId);
+
+accountEmail = 'altf4@gamil.com'
+accountPassword = "54321"
+accountCity = "mirzapur"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+because of issue in block scopre and functional 
+*/
