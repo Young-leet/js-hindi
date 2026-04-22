bfcda81b555e31c4d6c46ef98e11f2fd62f8ba63
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-22 08:51:58 +0000

    more convesion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,34 +1,72 @@
-let score = true
+// let score = true
 
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueInNumber = Number (score)
+// let valueInNumber = Number (score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
-
-
-// '33' = 33
-// '33abc' = NaN
-// true => 1 , false = 0
-// 
-
-let isLoggedin = 0
-let booleanIsLoggedIn = Boolean(isLoggedin)
-console.log(booleanIsLoggedIn)
-
-/* 
-1 => true
-0 => false
-'' => false
-'abc' => true
-*/
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
-let someNum = 25
-let stringNum = String(someNum)
-console.log(typeof stringNum)
+// // '33' = 33
+// // '33abc' = NaN
+// // true => 1 , false = 0
+// // 
+
+// let isLoggedin = 0
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn)
+
+// /* 
+// 1 => true
+// 0 => false
+// '' => false
+// 'abc' => true
+// */
 
 
+// let someNum = 25
+// let stringNum = String(someNum)
+// console.log(typeof stringNum)
+
+
+// 8888888888888888888888888 operations 888888888888888888888888888
+
+
+
+let value = 3
+let negval = -value
+console.log(negval)
+
+
+
+let str1 = 'aloo'
+let str2 = ' chat'
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log('1'+'2')
+// console.log('1'+'2'+3+3)
+// console.log(1+1+'2')
+
+// console.log( (3+4)*5 % 3  )
+
+
+// console.log(+true)
+// console.log(+'')
+
+// let n1 , n2 , n3 
+
+// n1 = n2 = n3  = 2+2
+
+
+// console.table([n1,n2,n3])
+
+
+let gamecounter = 100 
+++gamecounter
+
+console.log(gamecounter)
