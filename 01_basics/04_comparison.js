07fc2690cbb9d9d0618f0a0f3764b494d4eab312
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-23 06:50:21 +0000

    datatypes knowlege

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2<1)
+// console.log(2!=1)
+// console.log(2==1)
+
+// console.log('2' > 1 )
+// console.log('02' > 1 )
+
+
+// console.log(null == 0 )
+// console.log(null >= 0 )
+
+
+
+
+
+// console.log(undefined == 0 )
+// console.log(undefined >= 0 )
+// console.log(undefined <= 0 )
+// console.log(undefined != 0 )
+
+
+// ================== strict chck ===================
+
+console.log(null === null )
+
+console.log('1'===1)
+console.log('0' === 0  )
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,86 @@
+// primitive datatype
+
+// 7 categories 
+
+/* 
+
+String
+number
+boolean
+null
+undefined
+symbol
+Bigint
+
+
+*/
+
+
+// refrence type (refrence )
+
+/*
+array 
+objects
+browser events
+functions
+*/ 
+
+
+const score = false
+
+const scoreValue = 100.3
+
+
+const scoreBool = false
+
+
+const scoreNull = null
+
+
+const scoredef = undefined
+
+let userEmail ;
+
+
+// 888888888888888888888888888888888888888888888888888888888888888888
+
+const id = Symbol( '123' )
+const anotherId = Symbol('123')
+
+console.log(id === anotherId )
+
+
+//********************************************************************* */
+
+const bignum = 1234123412134121412133412131412134n
+
+
+
+//arrays
+
+const heros = ['shaktiman' , 'nagraaj','jj']
+
+// objext
+
+let myobj = {
+    name : 'Nitin',
+    age : 44 ,
+
+
+}
+
+const myf = function(){
+    console.log('classic hellow')
+}
+
+
+console.log(typeof bignum)
+console.log(typeof scoreNull)
+console.log(typeof id)
+console.log(typeof anotherId)
+
+
+
+
+
+
