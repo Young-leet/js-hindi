16cff2a6a99b82e0b570aa8a5036893f1e123931
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-23 07:54:36 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,53 @@
+// const name  = 'dhruv'
+
+// const repo = 50 
+
+// console.log( repo+' value' )
+
+// console.log(`hello my name is ${name} and my repocount is ${repo} `)
+
+
+
+
+const gamename = new String('nitin-dc-com') 
+
+// console.log(typeof gamename)
+// console.log(gamename[0])
+// console.log(gamename.__proto__)
+// console.log(gamename.length)
+
+// console.log(gamename.length)
+
+// console.log(gamename.toUpperCase())
+
+// console.log(gamename.charAt(6))
+
+// console.log(gamename.indexOf('t'))
+
+
+
+// const newString = gamename.substring(1,4)
+// console.log(newString)
+
+
+
+// const anotherString = gamename.slice(-8,4)
+// console.log(anotherString)
+
+
+// const anotherString1 = '  nitin  '
+// console.log(anotherString1)
+// console.log(anotherString1.trim())
+
+
+
+// const url = 'http://nitin.com/nitin%20yadav'
+// console.log(url.replace('%20','-'))
+
+// console.log(url.includes('com'))
+
+
+
+
+
+console.log(gamename.split('-'))
