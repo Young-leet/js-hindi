ae489774c2b7ea653c71c839d7ae6e40d330c2e4
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-26 06:41:20 +0000

    arrays

--- 01_basics/07_dares.js
+++ 01_basics/07_dares.js
@@ -1,16 +1,55 @@
  // dates are calculated in ms
 // tc39 consortium javascript defines api
 
-let d = new Date()
-console.log(d.toString())
-console.log('----------------------------------');
+// let d = new Date()
+// console.log(d.toString())
+// console.log('----------------------------------');
 
-console.log(d.toDateString())
-console.log(d.toTimeString());
-console.log(d.toLocaleDateString());
-console.log(d.toISOString());
-console.log(d.toLocaleTimeString());
-
-
+// console.log(d.toDateString())
+// console.log(d.toTimeString());
+// console.log(d.toLocaleDateString());
+// console.log(d.toISOString());
+// console.log(d.toLocaleTimeString());
 
 
+// let mynewd = new Date(2024,1,26 , 5 ,3)
+// console.log(mynewd.toString())
+
+let cdate = new Date('2023-01-14')
+console.log(cdate.toLocaleDateString())
+
+let newd = new Date()
+console.log(newd);
+
+/***
+ * 0 : january
+ * 1 : february
+ */
+
+console.log(newd.getMonth())
+
+/** 0 : sunday 
+ * 1 : monday 
+ * 2: tuesday ..soon
+ */
+console.log(newd.getDay())
+
+console.log(newd.getDate())
+
+newd.toLocaleString('default' , { 
+    weekday : "long",
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+console.log();
---
+++ 01_basics/08_arrays.js
@@ -0,0 +1,27 @@
+
+// const mray1 = [0,1,2,3,4,5,true,"nitin"] ;
+// console.log(mray1);
+
+// // arrays do the shallow copy means arrays share same memory refrence
+
+// const dis = ['iu' , 'dahye' , 'jinjin' , 'pak']
+// console.log(dis[3]);
+
+
+let myray = []
+myray.push(77)
+myray.push(23)
+myray.push(244)
+myray.push(466)
+
+console.log(myray);
+
+console.log(myray.includes(31))
+
+
+
+// myray.unshift(8);
+// console.log(myray);
+// myray.shift()
+// console.log(myray);
+
