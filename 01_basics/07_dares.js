8106c3aad853fb116f7940209f886e7f04505523
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-26 05:37:38 +0000

    dates

--- 01_basics/07_dares.js
+++ 01_basics/07_dares.js
@@ -1,45 +1,16 @@
-let myDate = new Date()
+ // dates are calculated in ms
+// tc39 consortium javascript defines api
 
-// console.log(myDate);
-// console.log(myDate.toString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+let d = new Date()
+console.log(d.toString())
+console.log('----------------------------------');
 
-
-// let cdate = new Date(2023 , 0 , 23)
-// console.log(cdate.toDateString());
-
-
-// let ndate = new Date(2023 , 0 , 23 , 5 , 3)
-// console.log(ndate.toLocaleString());
+console.log(d.toDateString())
+console.log(d.toTimeString());
+console.log(d.toLocaleDateString());
+console.log(d.toISOString());
+console.log(d.toLocaleTimeString());
 
 
 
- let fdate = new Date("01-14-2023")
-// console.log(fdate.toLocaleString());
 
-
-// let myTimeStamp = Date.now()
-
-// console.log(myTimeStamp);
-
-// console.log(fdate.getTime());
-
-
-// console.log(Math.floor(Date.now()/1000));
-
-
-let newdate = new Date()
-
-console.log(newdate);
-
-console.log(newdate.getMonth()+1);
-
-console.log(newdate.getDay());
-
-newdate.toLocaleString('default',{
-
-    weekday : 'long'
-    
-})
