e396254726b541dc17a66e2b5a2619d8c03e532f
NITIN <102019082+Young-leet@users.noreply.github.com>
2026-04-30 15:38:37 +0000

    filter

---
+++ 04_iterations/five.js
@@ -0,0 +1,204 @@
+
+const coding = ['js' , 'ruby' , 'py' , 'cpp']
+
+
+// let val = coding.forEach((item)=>{
+//     return item
+// })
+
+// console.log(val);
+
+
+
+
+// coding.forEach(printme)
+
+// function printme(item){
+//     console.log(item);
+    
+// }
+
+
+// coding.forEach( (item , index , arr )=>{
+//     console.log(item , index , arr);
+    
+// })
+
+
+
+// const mycoding = [
+//     {
+//         languagename : 'javascript',
+//         languagefilename : 'js'
+//     },
+    
+//     {
+//         languagename : 'typescript',
+//         languagefilename : 'ts'
+//     },
+
+//     {
+//         languagename : 'python',
+//         languagefilename : 'py'
+//     }
+// ]
+
+// mycoding.forEach( (item)=>{
+//     console.log(item.languagename);
+//     console.log(item.languagefilename); 
+
+// })
+
+
+
+
+
+
+
+
+
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newnums = mynums.filter( (num) => num > 4 )
+// console.log(newnums);
+
+
+
+const newNums = []
+
+
+// mynums.forEach( (num) => {
+//         if(num > 4 ){
+//             newNums.push(num)
+//         }
+// })
+
+// console.log(newNums);
+
+
+
+// example of books
+
+/*** an array of objects */
+
+const collection = [
+    {
+        sno : 1 ,
+        cast : 'obc',
+        follow : 'nat'
+
+    }, {
+        sno : 2 ,
+        cast : 'gen',
+        follow : 'wolfee'
+
+    }, {
+        sno : 3 ,
+        cast : 'sc',
+        follow : 'zumi'
+
+    }, {
+        sno : 4 ,
+        cast : 'obc',
+        follow : 'jinny'
+
+    }, {
+        sno : 5 ,
+        cast : 'obc',
+        follow : 'nat'
+
+    }, {
+        sno : 6 ,
+        cast : 'obc',
+        follow : 'nat'
+
+    }, {
+        sno : 7 ,
+        cast : 'obc',
+        follow : 'nat'
+
+    }, {
+        sno : 8 ,
+        cast : 'obc',
+        follow : 'nat'
+
+    }, {
+        sno : 9 ,
+        cast : 'gen',
+        follow : 'wolfee'
+
+    }, {
+        sno : 10 ,
+        cast : 'gen',
+        follow : 'yuggiee'
+
+    }, {
+        sno : 11 ,
+        cast : 'gen',
+        follow : 'peeguu'
+
+    }, {
+        sno : 12 ,
+        cast : 'gen',
+        follow : 'wolfee'
+
+    }, {
+        sno : 13 ,
+        cast : 'gen',
+        follow : 'zumi'
+
+    }, {
+        sno : 14,
+        cast : 'gen',
+        follow : 'kiko'
+
+    }, {
+        sno : 15 ,
+        cast : 'gen',
+        follow : 'jinny'
+
+    }, {
+        sno : 16 ,
+        cast : 'sc',
+        follow : 'wolfee'
+
+    }, {
+        sno : 17 ,
+        cast : 'gen',
+        follow : 'peegu'
+
+    },
+     {
+        sno : 18 ,
+        cast : 'obc',
+        follow : 'jinny'
+
+    }, {
+        sno : 19,
+        cast : 'sc',
+        follow : 'peegu'
+
+    }, {
+        sno : 20,
+        cast : 'obc',
+        follow : 'kiko'
+
+    },
+]
+
+// const peeguFollow = collection.filter((data) => data.follow == 'peegu')
+
+// console.log(peeguFollow);
+
+
+// const kikoFollow = collection.filter((data) => data.follow == 'kiko')
+// console.log(kikoFollow);
+
+
+// const obccount = collection.filter((data) => data.cast == 'obc')
+// console.log(obccount);
+
+
+const natfollow = collection.filter((data) => data.follow == 'nat')
+console.log(natfollow);
--- 04_iterations/four.js
+++ 04_iterations/four.js
@@ -5,3 +5,67 @@ const myobj = {
     sw : 'swift by apple'
 }
 
+// for (const key in myobj) {
+
+// console.log(myobj[key]);
+
+// }
+
+
+// cart = ['js' , 'rb' , 'java' , 'cpp']
+
+// for (const key in cart) {
+//     console.log(cart[key]);
+    
+// }
+
+
+
+
+const map = new Map()
+map.set('in','India')
+map.set('us','united states of america')
+map.set('ch','china')
+
+
+for (const [key,value] in map) {
+    console.log(key , ':-' , value);   
+}
+
+
+/** objects are not iterable using forof loop
+ *  we iterate over objects using forin loop
+*/
+
+// const obj = {
+//     'game' : 'nfs',
+//     'game2' : 'spiderman'
+// }
+
+// for (const key in obj) {
+//     console.log(`${key} frpm obj corresponds to ${obj[key]}`)
+// }
+
+
+
+// for (const [key , value ] in obj){
+ 
+//     console.log('iterated obj using for in',obj[key] , ':-' , obj[value]);
+    
+
+// }
+    
+
+// for(const [g,n] of obj) {
+
+//         console.log(g , ':-' , n);  
+
+prog = ['vsync' , 'mmid' , 'cpp' , 'js' , 'vim']
+
+for( const k in prog){
+
+console.log(prog[k]);
+
+}
+
+
